e14bb570c961bba801d32a658c110e73adc58bef
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-11-15 21:16:41 -0800

    Started Working on the mob

--- game/Scripts/Player.gd
+++ game/Scripts/Player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 export var speed = 400
 var screen_size
@@ -10,6 +11,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -40,3 +42,15 @@ func _process(delta):
 	elif velocity.y !=0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deffered("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
